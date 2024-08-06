e6c4f5b47198f5baedc0e6c5472906483b4f7c5c
Ruslan T <lama.in.pijamas@gmail.com>
2024-08-06 23:15:51 +0200

    Bug fixes

--- Player/Prototype/character_body_2d.gd
+++ Player/Prototype/character_body_2d.gd
@@ -4,14 +4,10 @@ extends CharacterBody2D
 
 @export var dodge_speed : int = 175
 
-@export var ghost_node : PackedScene
-
 var dodging : bool = false
 var can_dodge : bool = true
 
 @onready var animation_tree : AnimationTree = $AnimationTree
-@onready var ghost_timer = $ghost_timer
-@onready var particles_2d = $GPUParticles2D
 
 var direction : Vector2 = Vector2.ZERO
 
@@ -26,7 +22,6 @@ func _physics_process(_delta):
 	
 	if Input.is_action_just_pressed("dodge") and can_dodge:
 		if(velocity != Vector2.ZERO):
-			dash()
 			dodging = true
 			can_dodge = false
 			animation_tree["parameters/conditions/jett_mode"] = true
@@ -42,20 +37,6 @@ func _physics_process(_delta):
 		velocity = Vector2.ZERO
 		
 	move_and_slide()
-	
-func add_ghost():
-	var ghost = ghost_node.instantiate()
-	ghost.set_property(position, $Sprite2D.scale)
-	get_tree().current_scene.add_child(ghost)
-
-func dash():
-	ghost_timer.start()
-	particles_2d.emitting = true
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", position + velocity, 0.45)
-	await tween.finished
-	ghost_timer.stop()
-	particles_2d.emitting = false
 	
 
 func update_animation_parameters():
@@ -78,7 +59,3 @@ func _on_dodge_time_timeout():
 
 func _on_dodge_cooldown_timeout():
 	can_dodge = true
-
-
-func _on_ghost_timer_timeout():
-	add_ghost()
